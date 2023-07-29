dcc722304e6e9df763ef3e4620aafa7a37bb8e2b
eN1nja <mr.buntar@gmail.com>
2023-07-29 13:05:07 +0300

    создал задачи

---
+++ Task_19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int num = 0;
+bool isValidInput = false;
+
+while (!isValidInput || num < 10000 || num > 99999)
+{
+    Console.Write("Введите пятизначное число: ");
+    isValidInput = int.TryParse(Console.ReadLine(), out num);
+
+    if (!isValidInput || num < 10000 || num > 99999)
+    {
+        Console.WriteLine("Число не пятизначное!");
+    }
+}
+
+if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)
+{
+    Console.WriteLine("Да.");
+}
+else
+{
+    Console.WriteLine("Нет.");
+}
---
+++ Task_21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, 
+// которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите число: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double num = Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2) + Math.Pow((Z2 - Z1), 2));
+System.Console.WriteLine(num);
